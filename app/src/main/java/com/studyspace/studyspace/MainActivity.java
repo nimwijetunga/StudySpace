79287e69ceb49a88adc59a29af95808c25b1ad15
vwijetunga <vnwijetu@uwaterloo.ca>
2018-05-26 05:10:42 -0400

    Added Finding User Location

--- app/src/main/java/com/studyspace/studyspace/MainActivity.java
+++ app/src/main/java/com/studyspace/studyspace/MainActivity.java
@@ -3,49 +3,52 @@ package com.studyspace.studyspace;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.studyspace.rest.client.GetData_UW;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String room_num = "1085", build_code = "MC";
-        GetData_UW data = new GetData_UW(this,build_code, room_num);
+        ctx = this;
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        try{
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                double [] origin = {location.getLongitude(), location.getLatitude()};
+                                String room_num = "1085", build_code = "MC";
+                                GetData_UW data = new GetData_UW(ctx,build_code, room_num);
+                                Log.d("Origin : ", origin[0] + " " + origin[1]);
+                            }
+                        }
+                    });
+        }catch(SecurityException e){
+            e.printStackTrace();
+        }
+
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    LocationManager myManager;
-                    MyLocListener loc;
-                    loc = new MyLocListener(this);
-                    myManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-                    try {
-                        myManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, loc );
-                    } catch (SecurityException e) {
-                        e.printStackTrace(); // lets the user know there is a problem with the gps
-                    }
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
+
+
+
+
 
 }
